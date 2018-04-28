4b805208cf2bee783069497e704f7a92b4c9ea2b
Vacko <vacko3@gmail.com>
2018-04-28 02:01:29 +0200

    Add CodeInject

---
+++ CodeInject/Bootstrap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CodeInject
+{
+    internal static class Bootstrap
+    {
+        #region Private Fields
+
+        private static bool m_initialized;
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        [STAThread]
+        private static int EntryPoint(string args)
+        {
+            if(m_initialized)
+                return 0;
+
+            m_initialized = true;
+
+            MessageBox.Show($"Managed code running inside [{Process.GetCurrentProcess().ProcessName}]");
+
+            return 0;
+        }
+
+        #endregion Private Methods
+    }
+}
